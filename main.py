a55501faedb0f618ce529f84c756af0161967ed9
OleksaSolo <9095945@gmail.com>
2023-12-02 11:44:32 +0100

    ver 1.01
    Start

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Path, Query, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from db import get_db, Note
+
+app = FastAPI()
+
+
+class NoteModel(BaseModel):
+    name: str
+    description: str
+    done: bool
+
+
+@app.post("/notes")
+async def create_note(note: NoteModel, db: Session = Depends(get_db)):
+    new_note = Note(name=note.name, description=note.description, done=note.done)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
+
+@app.get("/api/healthchecker")
+def healthchecker(db: Session = Depends(get_db)):
+    try:
+        # Make request
+        result = db.execute("SELECT 1").fetchone()
+        if result is None:
+            raise HTTPException(status_code=500, detail="Database is not configured correctly")
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
+
---
+++ readme.txt
@@ -0,0 +1,10 @@
+poetry add fastapi
+
+poetry add uvicorn[standard]
+
+uvicorn main:app --host localhost --port 8000 --reload
+
+docker (with postgres)
+
+
+
